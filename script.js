d36e395ebfe23407db656c8bcf96857a49ef3ab7
Roxas Ski <roxaskiGithub@gmail.com>
2025-01-22 12:03:29 +0000

    updated nav menu with a new design

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const menuToggle = document.querySelector('.hamburger-menu');
-const header = document.querySelector('header');
+let wrapper = document.querySelector('.wrapper');
+let linksWrapper = document.querySelector('.links-wrapper');
+let menu = document.querySelector('.hamburger-menu');
+let packages = document.querySelector('.packages');
+let events = document.querySelector('.events');
 
-menuToggle.onclick = function() {
-    header.classList.toggle('active');
-};
+//toggles hamburger menu
+menu.addEventListener('click', () => {
+    wrapper.classList.toggle('active')
+});
+
+//disables scroll when nav is open
+menu.addEventListener('click', () => {
+    document.body.classList.toggle('no-scroll');
+});
+
+//toggles packages drop-down sub menu
+packages.addEventListener('click', () => {
+    linksWrapper.classList.toggle('active-packages-menu');
+});
+
+//toggles events drop-down sub menu
+events.addEventListener('click', () => {
+    linksWrapper.classList.toggle('active-events-menu');
+});
