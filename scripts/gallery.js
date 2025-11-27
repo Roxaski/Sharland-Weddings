757212d0ab918e465879d039a6f5027c79eb370f
Roxas Ski <roxaskiGithub@gmail.com>
2025-11-27 11:13:41 +0000

    various improvements and adjustments

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,7 @@
-const gallery = document.querySelectorAll('.gallery img');
+const gallery = document.querySelector('.gallery');
+const galleryImgs = document.querySelectorAll('.gallery img');
 const lightbox = document.querySelector('.lightbox img');
+const lightboxSizes = '(max-width: 865px) 90vw, 800px';
 const previousBtn = document.querySelector('.previous');
 const nextBtn = document.querySelector('.next');
 const overlay = document.querySelector('.overlay');
@@ -36,9 +38,14 @@ gallery.addEventListener('keydown', (e) => {
 // preloads the previous and next image
 function preload(imageIndex) {
     // checks if the image is within the length of the gallery 
-    if (imageIndex >= 0 && imageIndex < gallery.length) {
-        // creates a new image element, whose source is equal to that of the gallery position, + or - 1 when called
-        new Image().src = gallery[imageIndex].src;
+    if (imageIndex >= 0 && imageIndex < galleryImgs.length) {
+        const img = galleryImgs[imageIndex];
+        const preloadImg = new Image();
+
+        // sets the image src, srcset and size for the browser to preload
+        preloadImg.srcset = img.srcset;
+        preloadImg.sizes = lightboxSizes;
+        preloadImg.src = img.src;
     };
 };
 
@@ -50,13 +57,20 @@ function displayOverlay() {
 
 // displays image & buttons
 function imagePreview() {
-    lightbox.src = gallery[currentImage].src;
     lightbox.classList.add('active');
+
+    // lightbox should use more appropriate image sizes
+    lightbox.srcset = galleryImgs[currentImage].srcset;
+    lightbox.sizes = lightboxSizes;
+    lightbox.alt = galleryImgs[currentImage].alt;
+
+    // fallback in case srcset and sizes isn't supported on the browser
+    lightbox.src = galleryImgs[currentImage].src;
 
     displayGalleryBtns();
 };
 
-// removes the buttons from the first and last gallery img
+// removes the buttons from the first and last lightbox images
 function displayGalleryBtns() {
     if(currentImage == 0) {
         previousBtn.style.display = 'none';
@@ -64,7 +78,7 @@ function displayGalleryBtns() {
         previousBtn.style.display = 'block';
     };
 
-    if(currentImage == gallery.length - 1) {
+    if(currentImage == galleryImgs.length - 1) {
         nextBtn.style.display = 'none';
     } else {
         nextBtn.style.display = 'block';
@@ -82,60 +96,96 @@ nextBtn.addEventListener('click', () => {
 previousBtn.addEventListener('click', () => {
     currentImage --;
     imagePreview();
-    preload(currentImage -1);
+    preload(currentImage - 1);
 });
 
-// removes overlay, image and buttons
+// removes overlay, image buttons, src and srcset along with alt
 function closeOverlay() {
-    document.body.style.overflow = 'auto';
+    document.querySelector('body').style.overflow = 'auto';
+    lightbox.src = '';
+    lightbox.srcset = '';
+    lightbox.alt = '';
     lightbox.classList.remove('active');
     overlay.style.display = 'none';
     nextBtn.style.display = 'none';
     previousBtn.style.display = 'none';
 };
 
+// closes the overlay
 overlay.addEventListener('click', () => {
     closeOverlay();
 });
 
+// stores the value of the timeout ID which is used to delay the image resize from triggering too often
+let timeoutID;
+
+window.addEventListener('resize', () => {
+    // clears the timeoutID after it's initially triggered
+    clearTimeout(timeoutID);
+
+    // triggers the timeout only if the lightbox is displayed
+    if(overlay.style.display == 'block') {
+        // added active class to gallery to help with flickering during resize
+        gallery.classList.add('active');
+
+        // re-render image then show the content after a short delay set by the timeout below
+        timeoutID = setTimeout(() => {
+            imagePreview();
+
+            // removes the active class on the next repaint
+            requestAnimationFrame(() => {
+                gallery.classList.remove('active');
+            });
+        }, 100); // 100ms delay before the next resize can happen
+    }
+});
+
+// stores the value of where the finger touches the screen
+let touchStart;
+
 lightbox.addEventListener('touchstart', (e) => {
-    // stops the default event from happening (swiping from the edge of the screenmakes the page go back)
+    // stops the default event from happening (swiping from the edge of the screen makes the page go back)
     e.preventDefault();
     
     // returns the location of where the touch started on the X-axis
     touchStart = e.touches[0].clientX;
 });
 
-lightbox.addEventListener('touchend', (e) => {
+//  stores the value of when the finger stops on the screen
+let touchEnd;
 
+lightbox.addEventListener('touchend', (e) => {
     // returns the location of where touch ended on the X-axis
     touchEnd = e.changedTouches[0].clientX;
 
+    // defines the minimum distance needed to swipe for next and previous images
     const minSwipe = 100;
+    // calculates the value to show the next or previous image
     const swipeToChangeImg = touchStart - touchEnd;
 
-    if(swipeToChangeImg > minSwipe && currentImage < gallery.length - 1) {
+    // if you swipe to the left and aren't on the last image, then it displays the next image
+    if(swipeToChangeImg > minSwipe && currentImage < galleryImgs.length - 1) {
         currentImage ++;
         imagePreview();
-        preload(currentImage +1);
-
+        preload(currentImage + 1);
+    // Else if you swipe to the right and aren't on the first image, then it displays the previous image
     } else if (swipeToChangeImg < -minSwipe && currentImage > 0) {
         currentImage --;
         imagePreview();
-        preload(currentImage -1);
+        preload(currentImage - 1);
     };
 });
 
 // image navigation using the arrows keys
 window.addEventListener('keydown', (e) => {
-    if(overlay.style.display == 'block' && e.key == 'ArrowRight' &&  currentImage < gallery.length - 1) {
+    if(overlay.style.display == 'block' && e.key == 'ArrowRight' &&  currentImage < galleryImgs.length - 1) {
         currentImage ++;
         imagePreview();
-        preload(currentImage +1);
+        preload(currentImage + 1);
     } else if (overlay.style.display == 'block' && e.key == 'ArrowLeft' && currentImage > 0) {
         currentImage --;
         imagePreview();
-        preload(currentImage -1);
+        preload(currentImage - 1);
     } else if (e.key == 'Escape') {
         closeOverlay();
     };
